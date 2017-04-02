62dfd8b923d7887d4bea5f15a2d7438cd787bf65
Shantanu Chakrawarty <51011922+shantanuuchak@users.noreply.github.com>
2017-04-02 15:14:42 +0000

    targeted elements

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
 // Targetin DOM
+
+const firstScoreText = document.querySelector(".first .score");
+const firstBtnAdd1 = document.querySelector(".first .add-one");
+const firstBtnAdd2 = document.querySelector(".first .add-two");
+const firstBtnAdd3 = document.querySelector(".first .add-three");
+
+const secondScoreText = document.querySelector(".second .score");
+const secondBtnAdd1 = document.querySelector(".second .add-one");
+const secondBtnAdd2 = document.querySelector(".second .add-two");
+const secondBtnAdd3 = document.querySelector(".second .add-three");
